a17ce08b7df5bd839663acbf5c39dbb659405e7c
xxxJOKERxxx <0888200@gmail.com>
2025-06-25 23:55:21 +0300

    Добавил двумерный массив

--- src/Main.java
+++ src/Main.java
@@ -120,7 +120,7 @@ public class Main {
 
         System.out.println("___________________");
         // двумерный массив
-        int[][] arr2 = new int[5][2];
+        int[][] arr2 = new int[5][5];
         for(int i = 0; i < arr2.length; i++) {
             for(int j = 0; j < arr2[i].length; j++) {
                 System.out.print(arr2[i][j] + " ");
